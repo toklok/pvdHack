9090979534595e63da3cb323490a5492eb9bdedd
Kathleen <kayjsmyth@gmail.com>
2016-07-23 13:59:51 -0400

    fixed location of map var
    added map var to document.ready

--- js/main.js
+++ js/main.js
@@ -2,13 +2,20 @@ var $ = require('jquery');
 
 var url = 'https://na-georss.waze.com/rtserver/web/TGeoRSS?tk=ccp_partner&ccp_partner_name=Providence&format=JSON&types=traffic,alerts,irregularities&polygon=-71.440487,41.866467;-71.374741,41.858029;-71.375427,41.828744;-71.395512,41.810450;-71.373367,41.785033;-71.405811,41.764486;-71.445465,41.786057;-71.446238,41.801095;-71.489410,41.817281;-71.440487,41.866467;-71.440487,41.866467';
 
+
 $(document).ready(function () {
+  
+  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  
   $.ajax(url, {
+    
     success: function (data) {
       console.log(data);
     },
+  
     error: function (err) {
       console.log('oh noes');
     }
+    
   })
 });
