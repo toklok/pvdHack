cc559a736f166c8d7b4da39355235e2266b0b07d
Joseph Curtis <joecurt85@gmail.com>
2016-07-23 15:14:58 -0400

    add files

--- js/map.js
+++ js/map.js
@@ -1,23 +1,6 @@
-  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-  var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-  };
+foo = function () {
+    console.log('map');
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
   
-  L.geoJson(geojsonFeature).addTo(map);
-  
-  var myPoints = [{
-    "type": "Point",
-    "coordinates": [-104.99404, 39.75621]
-}, {
-    "type": "Point",
-    "coordinates": [-104.99404, 39.75234]
-}];
+     L.tileLayer('https://{s}.tiles.mapbox.com/v3/examples.map-i86knfo3/{z}/{x}/{y}.png').addTo(mymap)
+};
